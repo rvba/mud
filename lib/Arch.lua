726124d78467ec81a74a34d2e53386594b47c6b0
rvba <mil01@free.fr>
2015-08-20 17:00:33 +0200

    arch: add grid

--- lib/Arch.lua
+++ lib/Arch.lua
@@ -6,7 +6,44 @@ local Arch = {}
 
 
 function Arch:print()
-	print("Arch!")
+	for j=1,self.y do
+		for i=1,self.x do
+				print( "point ", 
+					"i ", i, "j ", j, "n", i+j,
+					"[", self.grid[i+j].x,
+					self.grid[i+j].y,
+					"]"
+					)
+			end
+		end
+end
+
+function Arch:make_grid(x,y)
+
+	self.grid={}
+	self.x=x
+	self.y=y
+	n = 1
+	for j=1,y do
+		for i=1,x do
+				self.grid[n] = {}
+				self.grid[n].x = i
+				self.grid[n].y = j
+				n = n + 1
+			end
+		end
+end
+
+function Arch:build_grid()
+	n = 1
+	for j=1,self.y do
+		for i=1,self.x do
+				x = self.grid[n].x
+				y = self.grid[n].y
+				self:add_vertex(x,y,0)
+				n = n + 1
+			end
+		end
 end
 
 function Arch:new()
@@ -15,6 +52,7 @@ function Arch:new()
 	setproto(arch,self,"arch")
 	return arch
 end
+
 
 local _Arch = Arch
 return _Arch
--- sketch/arch.lua
+++ sketch/arch.lua
@@ -1,4 +1,7 @@
 -- arch
 local Arch = require "Arch"
 local arch = Arch:new()
+arch:make_grid(5,5)
+arch:build_grid()
 arch:print()
+arch:build_object()
