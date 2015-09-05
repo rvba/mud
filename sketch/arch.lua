d1cf38cd3b7bf2c9b0cca8c274812fb908891804
rvba <mil01@free.fr>
2015-09-05 10:16:16 +0200

    change arch sketch

--- sketch/arch.lua
+++ sketch/arch.lua
@@ -1,8 +1,8 @@
 -- arch
 local Arch = require "Arch"
 local arch = Arch:new()
-arch:make_grid(30,30)
-arch:add_seed(15,15)
+arch:make_grid(4,4)
+arch:add_seed(2,2)
 arch:grow()
 arch:update()
 arch:build_object()
