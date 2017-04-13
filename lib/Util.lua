efa9145ba179ea3221f03b4e41e71e02a45427ca
rvba <mil01@free.fr>
2017-04-13 22:20:01 +0200

    move timer to Util.lua

--- lib/Util.lua
+++ lib/Util.lua
@@ -1,7 +1,44 @@
 
 local names = {}
+local records = {} 
 
 local _M = {}
+
+
+local function clock(name,interval)
+
+	local now = os.time()
+	record = records[name]
+	if not record then
+		records[name] = { start = now, interval = interval }
+		return false
+	else
+		if now - record["start"] >= interval then
+			record["start"] = now
+			return true
+		else
+			return false
+		end
+	end
+end
+
+local function delta(name)
+	local now = os.time()
+	record = records[name]
+	if record then
+		return now - record["start"] 
+	else
+		return 0
+	end
+end
+
+local function test()
+	if timer.clock("test",3) then
+		print("clock!")
+	end
+end
+
+
 local function set_name(name)
 
 	local entry = names[name]
@@ -24,6 +61,11 @@ local function round(num, numDecimalPlaces)
 
 _M.set_name = set_name
 _M.round = round
+_M.clock = clock
+_M.delta = delta
+_M.test = test
+
+
 return _M
 
 
