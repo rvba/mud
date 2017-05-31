8329d49234588a1c5c380cef85001297283a0804
rvba <mil01@free.fr>
2017-05-31 21:06:07 +0200

    frame: use self

--- lib/Frame.lua
+++ lib/Frame.lua
@@ -89,10 +89,10 @@ function Frame:add_frame()
 	v7:add(vt4)
 
 	-- front outer points
-	local p0 = frame:add_vertex(vo.x,vo.y,vo.z)
-	local p1 = frame:add_vertex(v1.x,v1.y,v1.z)
-	local p2 = frame:add_vertex(v2.x,v2.y,v2.z)
-	local p3 = frame:add_vertex(v3.x,v3.y,v3.z)
+	local p0 = self:add_vertex(vo.x,vo.y,vo.z)
+	local p1 = self:add_vertex(v1.x,v1.y,v1.z)
+	local p2 = self:add_vertex(v2.x,v2.y,v2.z)
+	local p3 = self:add_vertex(v3.x,v3.y,v3.z)
 
 	local pp0 = p0
 	local pp1 = p1
@@ -100,10 +100,10 @@ function Frame:add_frame()
 	local pp3 = p3
 
 	--front inner points
-	local p4 = frame:add_vertex(v4.x,v4.y,v4.z)
-	local p5 = frame:add_vertex(v5.x,v5.y,v5.z)
-	local p6 = frame:add_vertex(v6.x,v6.y,v6.z)
-	local p7 = frame:add_vertex(v7.x,v7.y,v7.z)
+	local p4 = self:add_vertex(v4.x,v4.y,v4.z)
+	local p5 = self:add_vertex(v5.x,v5.y,v5.z)
+	local p6 = self:add_vertex(v6.x,v6.y,v6.z)
+	local p7 = self:add_vertex(v7.x,v7.y,v7.z)
 
 	local pp4 = p4
 	local pp5 = p5
@@ -111,10 +111,10 @@ function Frame:add_frame()
 	local pp7 = p7
 
 	-- front faces
-	frame:add_face(p0,p4,p5,p1)
-	frame:add_face(p1,p5,p6,p2)
-	frame:add_face(p2,p6,p7,p3)
-	frame:add_face(p3,p7,p4,p0)
+	self:add_face(p0,p4,p5,p1)
+	self:add_face(p1,p5,p6,p2)
+	self:add_face(p2,p6,p7,p3)
+	self:add_face(p3,p7,p4,p0)
 
 
 	-- rear vectors
@@ -129,34 +129,34 @@ function Frame:add_frame()
 	v6:add(vcross)
 	v7:add(vcross)
 
-	p0 = frame:add_vertex(vo.x,vo.y,vo.z)
-	p1 = frame:add_vertex(v1.x,v1.y,v1.z)
-	p2 = frame:add_vertex(v2.x,v2.y,v2.z)
-	p3 = frame:add_vertex(v3.x,v3.y,v3.z)
+	p0 = self:add_vertex(vo.x,vo.y,vo.z)
+	p1 = self:add_vertex(v1.x,v1.y,v1.z)
+	p2 = self:add_vertex(v2.x,v2.y,v2.z)
+	p3 = self:add_vertex(v3.x,v3.y,v3.z)
 
-	p4 = frame:add_vertex(v4.x,v4.y,v4.z)
-	p5 = frame:add_vertex(v5.x,v5.y,v5.z)
-	p6 = frame:add_vertex(v6.x,v6.y,v6.z)
-	p7 = frame:add_vertex(v7.x,v7.y,v7.z)
+	p4 = self:add_vertex(v4.x,v4.y,v4.z)
+	p5 = self:add_vertex(v5.x,v5.y,v5.z)
+	p6 = self:add_vertex(v6.x,v6.y,v6.z)
+	p7 = self:add_vertex(v7.x,v7.y,v7.z)
 
-	frame:add_face(p0,p4,p5,p1)
-	frame:add_face(p1,p5,p6,p2)
-	frame:add_face(p2,p6,p7,p3)
-	frame:add_face(p3,p7,p4,p0)
+	self:add_face(p0,p4,p5,p1)
+	self:add_face(p1,p5,p6,p2)
+	self:add_face(p2,p6,p7,p3)
+	self:add_face(p3,p7,p4,p0)
 
 	-- inner sides
 	--
-	frame:add_face(p4,p5,pp5,pp4)
-	frame:add_face(p5,p6,pp6,pp5)
-	frame:add_face(p6,p7,pp7,pp6)
-	frame:add_face(p7,p4,pp4,pp7)
+	self:add_face(p4,p5,pp5,pp4)
+	self:add_face(p5,p6,pp6,pp5)
+	self:add_face(p6,p7,pp7,pp6)
+	self:add_face(p7,p4,pp4,pp7)
 
 	-- outer sides
 	
-	frame:add_face(p0,p1,pp1,pp0)
-	frame:add_face(p0,p3,pp3,pp0)
-	frame:add_face(p3,p2,pp2,pp3)
-	frame:add_face(p1,p2,pp2,pp1)
+	self:add_face(p0,p1,pp1,pp0)
+	self:add_face(p0,p3,pp3,pp0)
+	self:add_face(p3,p2,pp2,pp3)
+	self:add_face(p1,p2,pp2,pp1)
 
 end
 
