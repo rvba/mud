989c18dc79adc334dfbf7b1131dea2453018a79f
rvba <mil01@free.fr>
2017-09-01 10:33:03 +0200

    cleanup

--- lib/luajit/build.sh
+++ lib/luajit/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "[BUILD LUAJIT]"
 ls
-#src_dir=/home/milovann/SOURCES/MINUIT/modus/src/mud/lib/luajit
-#build_dir=/home/milovann/SOURCES/MINUIT/modus-build/luajit
 src_dir="$1"/src/mud/lib/luajit
 build_dir="$2"/luajit
 echo $src_dir
